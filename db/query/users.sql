c517ae9e907be213acfe0f0ba09dc63c94ff9ee1
Leandro <leoest.dev@gmail.com>
2022-07-11 18:30:16 -0500

    generate CRUD operations

--- db/query/users.sql
+++ db/query/users.sql
@@ -5,3 +5,23 @@ INSERT INTO users (
   $1, $2, $3, $4, $5
 )
 RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users
+WHERE username = $1;
+
+-- name: ListUsers :many
+SELECT * FROM users
+ORDER BY username
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateUser :one
+UPDATE users
+SET total_expenses = $2
+WHERE username = $1
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE username = $1;
